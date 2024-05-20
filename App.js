941d95e55f85c8b142bcfc1c8262f4e02d254194
nishant7412 <nsnishant5930@gmail.com>
2024-05-20 15:36:36 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{ id:"child"},[
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag"),
-    
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag"),
-    ]),
-]);
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+import ReactDOM from "react-dom/client";
+
+// react element
+ const jsxHeading = <h1 id="heading">Hello am here</h1>;
+ console.log(jsxHeading);
+//react component 
+//class based component - old
+
+//functional based component - new way
+// a function that return some piece of jsx code is functional component
+const HeadinOnly =()=>(
+    <div id="contain">
+        <h1 className="head"> Hello from  component compostion</h1>
+    </div>
+)
+
+const HeadingComponet =()=>(
+    <div id="contain">
+        <HeadinOnly/>
+        {HeadinOnly()}
+        <h1 className="head"> Hello from functional component </h1>
+    </div>
+)
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(<HeadingComponet />);
+ 
